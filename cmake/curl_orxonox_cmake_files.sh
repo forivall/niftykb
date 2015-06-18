847c7d67117214ec107d373821ab659d86031e17
Jordan Klassen <jordan@klassen.me.uk>
2015-06-17 19:00:52 -0700

    use orxonox cmake modules

---
+++ cmake/curl_orxonox_cmake_files.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl -OL http://www.orxonox.net/export/2710/code/trunk/cmake/FindDirectX.cmake
+curl -OL http://www.orxonox.net/export/2710/code/trunk/cmake/FindPackageHandleAdvancedArgs.cmake
+curl -OL http://www.orxonox.net/export/2710/code/trunk/cmake/HandleLibraryTypes.cmake
+curl -OL http://www.orxonox.net/export/2710/code/trunk/cmake/CompareVersionStrings.cmake
